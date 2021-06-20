4031e8085884615ff42bfe182bfeef392cc6b387
Victor Martins <hidiku_lhiku@hotmail.com>
2021-06-20 01:50:35 -0300

    Subindo código de troca de icones em javascript.
    Inserindo código de troca de icones de acordo com o tema do navegador.

--- src/ArchLinux/wwwroot/js/site.js
+++ src/ArchLinux/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function setupIcons() {
+    const lightSchemeIcon = document.querySelector('link#light-scheme-icon');
+    const darkSchemeIcon = document.querySelector('link#dark-scheme-icon');
 
-// Write your JavaScript code.
+    function setLight() {
+        document.head.append(lightSchemeIcon);
+        darkSchemeIcon.remove();
+    }
+
+    function setDark() {
+        lightSchemeIcon.remove();
+        document.head.append(darkSchemeIcon);
+    }
+
+
+    const matcher = window.matchMedia('(prefers-color-scheme:dark)');
+    function onUpdate() {
+        if (matcher.matches) {
+            setDark();
+        } else {
+            setLight();
+        }
+    }
+    matcher.addListener(onUpdate);
+    onUpdate();
+}
+
+setupIcons();
